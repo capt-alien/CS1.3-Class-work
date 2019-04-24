f3a48586f7770fb011e7129d40785df11a224922
capt-alien <eric.botcher@presidio.edu>
2019-04-24 15:07:15 -0700

    started on sets

---
+++ sets.py
@@ -0,0 +1,39 @@
+"""Help credit: Steve"""
+from hashtable import HashTable
+
+class Set(object):
+
+
+    #init method adds elements in sequence
+    def __init__(self, elements= None):
+        # init as hash HashTable
+        self.hash_set = HashTable
+        if elements != None:
+            self.add(item)
+
+#Operations on single element:
+    def size(self, element):
+        return self.hash_set.size
+
+    def add(self, element):
+        pass
+
+    def remove(self, element):
+        pass
+
+    def contains(self, element):
+        pass
+
+#Operations on another set
+
+    def union(self, set_b):
+        pass
+
+    def intersection(self, set_b):
+        pass
+
+    def difference(self, set_b):
+        pass
+
+    def is_subset(self, set_b):
+        pass
