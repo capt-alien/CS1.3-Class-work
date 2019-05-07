f8a15d80b8e3dc4e19d477b57f4599b04366ab12
capt-alien <eric.botcher@presidio.edu>
2019-05-06 22:11:05 -0700

    A

--- sets.py
+++ sets.py
@@ -5,14 +5,16 @@ class Set(object):
     def __init__(self, elements=None):
         # init as hash HashTable
         self.hash_set = HashTable()
+        #Size properity within the set
+        self.size = 0
         if elements != None:
             for item in elements:
                 self.add(item)
 
 #Operations on single element:
-    def size(self, element):
-        """Best/worst case: O(n)"""
-        return self.hash_set.size
+    def size(self):
+        """Best/worst case: O(1)"""
+        return self.size
 
     def contains(self, element):
         """Best/worst case: O(n)"""
@@ -24,51 +26,61 @@ class Set(object):
         #adds an element to the set
         if self.contains(element) == False:
             self.hash_set.set(element, value = None)
+            self.size += 1
 
     def remove(self, element):
         """Best/worst case: O(n)"""
         if self.contains(element) == True:
             self.hash_set.delete(element)
+            self.size -= 1
 
+
+# Your union, intersection, and difference methods
+# should create and return a new set, without altering
+# your set or the given set. Currently, you are initializing
+ # a HashTable instead of a new Set, and altering your
+  # original set to hold the new values. Change this to
+  # add values to and return a new set!
 
 #Operations on another set
     def union(self, set_b):
         """Returns a new set that is the union of two sets
         Best/worst case: O(n)"""
-
-        temp = HashTable()
-        for item in self.hash_set.keys():
-            temp.set(item, None)
-        for item in set_b.hash_set.keys():
-            temp.set(item, None)
-        self.hash_set = temp
-        return self
-
+        union_set = Set()
+        for item in self:
+            union_set.add(item)
+        for item in set_b:
+            union_set.add(item)
+        return union_set
 
     def intersection(self, set_b):
         """Returns a new set as intersetction of two sets
-                Best/worst case: O(n)"""
-        temp = HashTable()
-        for item in self.hash_set.keys():
-            if set_b.hash_set.contains(item):
-                temp.set(item, None)
-        self.hash_set = temp
-        return self
+                Best/worst case: O(n)
+                Yes, Anisha I drew inspiration from your code"""
+        i_set = Set()
+        smaller = self
+        bigger = set_b
+        if len(self) > len(set_b):
+            bigger = self
+            smaller = set_b
+        for item in smaller:
+            if item in bigger:
+                i_set.add(item)
+        return i_set
 
     def difference(self, set_b):
         """Returns a new set that is the difference between the two sets
             Best/worst case: O(n)"""
-        temp = HashTable()
+        diff_set = set()
         #for self
-        for item in self.hash_set.keys():
+        for item in self:
             if not set_b.contains(item):
-                temp.set(item, None)
+                diff_set.add(item)
         #for set_b
         for item in set_b.hash_set.keys():
-            if not self.contains(item):
-                temp.set(item, None)
-        self.hash_set = temp
-        return self
+            if not self:
+                diff_set.add(item)
+        return diff_set
 
     def is_subset(self, set_b):
         """Returns a boolian indicating if one set has all the elements
@@ -87,6 +99,6 @@ class Set(object):
 if __name__ == '__main__':
     set_a = Set(['a', 'b', 'c'])
     set_b = Set(['b', 'c', 'x', 'y'])
-    diff_set = set_a.difference(set_b)
-    for x in diff_set:
-        print(x)
+    # diff_set = set_a.union(set_b)
+    for x in set_b:
+        print(set_b)
--- sets_test.py
+++ sets_test.py
@@ -6,45 +6,45 @@ class SetTest(unittest.TestCase):
     #test each part of the function
     def test_init(self):
         test_set = Set()
-        assert test_set.hash_set.size == 0
+        assert test_set.size == 0
 
     def test_size(self):
         test_set = Set()
-        assert test_set.hash_set.size == 0
+        assert test_set.size == 0
         test_set.add('a')
-        assert test_set.hash_set.size == 1
+        assert test_set.size == 1
         test_set.add('b')
-        assert test_set.hash_set.size == 2
+        assert test_set.size == 2
         test_set.add('c')
-        assert test_set.hash_set.size == 3
+        assert test_set.size == 3
         test_set.add('d')
-        assert test_set.hash_set.size == 4
+        assert test_set.size == 4
         test_set.add('e')
-        assert test_set.hash_set.size == 5
+        assert test_set.size == 5
         #Edge Cases
         test_set.add('')
-        assert test_set.hash_set.size == 6
+        assert test_set.size == 6
         test_set.add(1)
-        assert test_set.hash_set.size == 7
+        assert test_set.size == 7
         test_set.add('word')
-        assert test_set.hash_set.size == 8
+        assert test_set.size == 8
         test_set.add('!')
-        assert test_set.hash_set.size == 9
+        assert test_set.size == 9
         test_set.add('this is a larger string test')
-        assert test_set.hash_set.size == 10
+        assert test_set.size == 10
         test_set.add(55)
-        assert test_set.hash_set.size == 11
+        assert test_set.size == 11
         test_set.add(1.56)
-        assert test_set.hash_set.size == 12
+        assert test_set.size == 12
         #test to make sure it doesnt add duplicates
         test_set.add('a')
-        assert test_set.hash_set.size == 12
+        assert test_set.size == 12
         test_set.add(1)
-        assert test_set.hash_set.size == 12
+        assert test_set.size == 12
         test_set.add(1.56)
-        assert test_set.hash_set.size == 12
+        assert test_set.size == 12
         test_set.add('word')
-        assert test_set.hash_set.size == 12
+        assert test_set.size == 12
 
     def test_contains(self):
         test_set = Set(['a', 'b', 'c', 1, 2.1, "word"])
@@ -63,43 +63,43 @@ class SetTest(unittest.TestCase):
 
     def test_add(self):
         test_set = Set()
-        assert test_set.hash_set.size == 0
+        assert test_set.size == 0
         test_set.add('a')
-        assert test_set.hash_set.size == 1
+        assert test_set.size == 1
         assert test_set.contains('a') ==True
         test_set.add('a')
-        assert test_set.hash_set.size == 1
+        assert test_set.size == 1
         assert test_set.contains('a') ==True
         test_set.add('b')
-        assert test_set.hash_set.size == 2
+        assert test_set.size == 2
         assert test_set.contains('b') ==True
         #test for int
         test_set.add(12)
-        assert test_set.hash_set.size == 3
+        assert test_set.size == 3
         assert test_set.contains(12) ==True
         test_set.add(12)
-        assert test_set.hash_set.size == 3
+        assert test_set.size == 3
         assert test_set.contains(12) ==True
         #test for float
         test_set.add(1.123)
-        assert test_set.hash_set.size == 4
+        assert test_set.size == 4
         assert test_set.contains(1.123) ==True
         test_set.add(1.123)
-        assert test_set.hash_set.size == 4
+        assert test_set.size == 4
         assert test_set.contains(12) ==True
         #test for long string
         test_set.add("word to your mother")
-        assert test_set.hash_set.size == 5
+        assert test_set.size == 5
         assert test_set.contains("word to your mother") ==True
         test_set.add("word to your mother")
-        assert test_set.hash_set.size == 5
+        assert test_set.size == 5
         assert test_set.contains("word to your mother") ==True
 
     def test_remove(self):
         test_set = Set(['a','b','c'])
-        assert test_set.hash_set.size == 3
+        assert test_set.size == 3
         test_set.remove('b')
-        assert test_set.hash_set.size == 2
+        assert test_set.size == 2
 
 
 # TEst for multiple sets
